afbcacd7d33363ffffa26c4d47d55ca9488dbd2c
Denys Semanchuk <semanchukdenys@icloud.com>
2025-02-05 16:44:51 +0100

    added authorizedHeader component

--- app/providers/AuthProvider.tsx
+++ app/providers/AuthProvider.tsx
@@ -3,18 +3,26 @@ import { useDispatch, useSelector } from 'react-redux'
 import { checkAuthToken } from 'store/thunks/authThunks';
 import { RootState } from 'types';
 import { AppDispatch } from '../../pages/login';
+import { AuthHeader } from 'components/AuthorizedHeader/AutorizedHeader';
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
   const dispatch = useDispatch<AppDispatch>()
-  const { loading } = useSelector((state: RootState) => state.auth)
+  const { loading,isAuthenticated } = useSelector((state: RootState) => state.auth)
 
   useEffect(() => {
     dispatch(checkAuthToken())
   }, [])
 
   if (loading) {
-    return <div>Loading...</div>
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900" />
+      </div>
+    )
   }
 
-  return <>{children}</>
+  return <>
+  {isAuthenticated && <AuthHeader />}
+  {children}
+</>
 }
