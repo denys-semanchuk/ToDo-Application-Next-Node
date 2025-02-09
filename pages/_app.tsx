01270729c856906581c36fc7ba5fc051f3241e3c
Denys Semanchuk <semanchukdenys@icloud.com>
2025-02-09 18:37:14 +0100

    updated _app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,23 @@
 import { AppProps } from 'next/app';
-import './globals.css';
 import { Provider } from 'react-redux';
-import { store } from './../app/store/index';
-import { AuthProvider } from 'providers/AuthProvider';
-import { Footer } from 'components/Footer/Footer';
+import { store } from '../app/store/index';
+import { AppProvider } from 'providers/AppProvider';
 import { Header } from 'components/Header/Header';
+import { Footer } from 'components/Footer/Footer';
+import './globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <AuthProvider>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </AuthProvider>
+      <AppProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </div>
+      </AppProvider>
     </Provider>
   );
 }
