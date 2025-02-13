2ad758f1f56d4ab8cad55be67deb07ed8ef00cd9
Denys Semanchuk <semanchukdenys@icloud.com>
2025-02-13 21:15:26 +0100

    added favicon

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,7 +1,19 @@
-"use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Todo App',
+  description: 'A modern task management application',
+  icons: {
+    icon: [
+      {
+        url: '/assets/favicon.svg',
+        type: 'image/svg+xml',
+      }
+    ]
+  }
+}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
