bb0ba113232dfa5af08df87acee1921ef186b1a2
denys-semanchuk <semanchukdenys@icloud.com>
2025-04-09 12:45:56 +0200

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <body className="bg-gray-50">
         <Main />
